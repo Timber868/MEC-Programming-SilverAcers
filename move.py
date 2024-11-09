6f35d09cf796195705bcc89694c71ce187c4e22e
Victor Odoux <victorodoux2004@gmail.com>
2024-11-09 15:10:00 -0500

    move_score

--- move.py
+++ move.py
@@ -1,11 +1,12 @@
 import piece
 class Move:
 
-    def __init__(self, piece, position, orientation):
+    def __init__(self, piece, position, orientation, board):
 
         self.piece = piece
         self.position = position
         self.orientation = orientation
+        self.score = self.move_score(board)
 
     def __repr__(self):
        # string representation
@@ -21,6 +22,17 @@ class Move:
             "y": self.position[1],
             "orientation": self.orientation
         }
+    
+    def move_score(self, board):
+        piece_score= self.piece.points
+        piece_position= self.piece.position
+        difference_x= abs(10-piece_position[0])
+        difference_y= abs(10-piece_position[1])
+        difference_center=difference_x + difference_y
+        piece_shape= self.piece.shape
+        for i in range(len(piece_shape)):
+            return
+        return ((piece_score/5)*0.7+(difference_center/10)*0.3)*100
 
 
 
